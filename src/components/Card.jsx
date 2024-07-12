e7e0ec307b545e1031b0fc8c5a3fd55978feeaee
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-07-12 14:47:36 +0530

    padding edit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -24,21 +24,21 @@ const Card = () => {
         <section className='py-10'>
             <div className="container px-3 py-0.5 mb-px">
                 <div className="bg-light_gray p-5 lg:p-10 flex flex-col items-center">
-                    <div className="flex items-center justify-center gap-1 lg:gap-3">
+                    <div className="flex items-center justify-center gap-1 sm:gap-3">
                         <span className='sm:w-[60px] w-5 h-0.5 bg-light_green flex'></span>
-                        <h2 className='font-Kaushan font-normal text-xl lg:text-32 sm:mx-0.5 px-px text-light_green text-center lg:leading-46'>
+                        <h2 className='font-Kaushan font-normal text-xl sm:text-2xl md:text-32 sm:mx-0.5 px-px text-light_green text-center md:leading-46'>
                             Cutting Edge Technology
                         </h2>
                         <span className='sm:w-[60px] w-5 h-0.5 bg-light_green flex'></span>
                     </div>
-                    <h2 className='font-Archivo mt-2.5 max-w-[799px] text-center font-semibold text-2xl md:text-5xl md:leading-62 text-primary_black'>
+                    <h2 className='font-Archivo mt-2.5 max-w-[799px] text-center font-semibold text-2xl sm:text-3xl md:text-5xl md:leading-62 text-primary_black'>
                         Devices and Treatments to meet your needs
                     </h2>
-                    <div className="flex flex-col justify-center md:mt-14 mt-5 gap-10 pt-1 w-full h-full">
+                    <div className="flex flex-col justify-center md:flex-row items-center md:items-start md:mt-14 mt-5 gap-10 md:gap-8 lg:gap-10 pt-1 w-full h-full">
                         {cardData.map((card) => (
-                            <div key={card.id} className="max-w-[510px] w-full h-full">
-                                <div className="border-[#0000001A] border-[1.09px]">
-                                    <img src={card.image} alt={card.title} className='w-full' />
+                            <div key={card.id} className="max-w-[510px] w-full h-full min-h-[627px]">
+                                <div className="border-[#0000001A] border-[1.09px] h-full">
+                                    <img src={card.image} alt={card.title} className='w-full h-full xl:min-h-[390px]' />
                                     <div className="bg-white md:p-[30px] p-5">
                                         <p className='font-Archivo font-semibold text-primary_black text-25 leading-32'>
                                             {card.title}
